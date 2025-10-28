85209c590859ce168d053998a2089c3a0a5ce3ff
Neil C. Obremski <neil@pay-i.com>
2025-10-28 07:36:59 -0700

    Add CRON script

---
+++ python-cli/adverstorial-cron.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# Shell script run by cron to execute the adverstorial.py script
+# using wordlists stored in the parent directory.
+
+# get current directory of the script
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+PARENT_DIR="$(dirname "$SCRIPT_DIR")"
+WORDLISTS_DIR="$PARENT_DIR/wordlists"
+echo "Wordlists directory: $WORDLISTS_DIR"
+
+# get latest code
+pushd "$PARENT_DIR"; git pull; popd
+
+# for loop 5 times to generate 5 words
+words=""
+for i in {1..5}; do
+  # pick a random file from the wordlists directory
+  WORDLIST_FILE=$(find "$WORDLISTS_DIR" -type f | shuf -n 1)
+
+  # pick a random word from the selected file
+  RANDOM_WORD=$(shuf -n 1 "$WORDLIST_FILE")
+
+  if [ -z "$words" ]; then
+    words="$RANDOM_WORD"
+  else
+    words="$words $RANDOM_WORD"
+  fi
+done
+
+# pick random antagonist and protagonist from $PARENT_DIR/adversaries.txt file
+protagonist=$(shuf -n 1 "$PARENT_DIR/adversaries.txt")
+antagonist=$(shuf -n 1 "$PARENT_DIR/adversaries.txt")
+
+# pick a random temperature between 0.1 and 0.8
+temperature=$(awk -v min=0.1 -v max=0.8 'BEGIN{srand(); print sprintf("%.1f", min+rand()*(max-min))}')
+
+# pick a random number of rounds between 2 and 4
+rounds=$(shuf -i 2-4 -n 1)
+
+# echo "Generated words: $words"
+python3 "$SCRIPT_DIR/adverstorial.py" "$words" \
+  --antagonist "$antagonist" \
+  --protagonist "$protagonist" \
+  --temperature "$temperature" \
+  --rounds "$rounds"
